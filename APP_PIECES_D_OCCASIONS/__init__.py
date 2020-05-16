338d3346d764d85d73880338b002d4c020289aac
Rocco Ronzano <roccoronzano@hotmail.com>
2020-05-17 00:46:45 +0200

    added some files

---
+++ APP_PIECES_D_OCCASIONS/__init__.py
@@ -0,0 +1,21 @@
+# Un objet "obj_mon_application" pour utiliser la classe Flask
+# Pour les personnes qui veulent savoir ce que signifie __name__ une démonstration se trouve ici :
+# https://www.studytonight.com/python/_name_-as-main-method-in-python
+# C'est une chaîne de caractère qui permet de savoir si on exécute le code comme script principal
+# appelé directement avec Python et pas importé.
+from flask import Flask, flash, render_template
+from APP_PIECES_D_OCCASIONS.DATABASE import connect_db_context_manager
+
+
+# Objet qui fait "exister" notre application
+obj_mon_application = Flask(__name__, template_folder="templates")
+# Flask va pouvoir crypter les cookies
+obj_mon_application.secret_key = '_vogonAmiral_)?^'
+
+
+# Doit se trouver ici... soit après l'instanciation de la classe "Flask"
+# OM 2020.03.25 Tout commence ici par "indiquer" les routes de l'application.
+from APP_PIECES_D_OCCASIONS import routes
+from APP_PIECES_D_OCCASIONS.USERS import routes_gestion_users
+from APP_PIECES_D_OCCASIONS.GENDERS import routes_gestion_genders
+from APP_PIECES_D_OCCASIONS.GENDERS_FILMS import routes_gestion_genres_films
